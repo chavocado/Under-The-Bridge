737038ea8f97865f4603e378d8d330018033e22e
Daniel Chavez <d.chavez562@yahoo.com>
2016-07-20 18:49:01 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+//dependencies
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+//set port
+app.set('port', process.env.PORT || 5000);
+
+//serve back static files
+app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+//catch all
+app.use('/', index);
+
+//listen
+app.listen(app.get('port'), function() {
+  console.log('Listening on port: ', app.get('port'));
+});
